537392613cba1b0e185c29f9eff6dede38931639
zuk881 <zuk881@cox.net>
2020-03-21 12:50:33 -0700

    added seeds file

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+                        
+INSERT INTO Projects (projectName, projectTech, projectLink, projectImage) VALUES ("Burger Time", "MySQL, Node, Express, Handlebars", "https://hidden-island-01318.herokuapp.com/", "https://i.imgur.com/qxR15RA.jpg");
+
+INSERT INTO Projects (projectName, projectTech, projectLink, projectImage) VALUES ("GifTastic", "JavaScript, CSS, HTML, Bootstrap 4", "https://zuk881.github.io/GifTastic/", "https://i.imgur.com/Dr4YG30.jpg");
+
+INSERT INTO Projects (projectName, projectTech, projectLink, projectImage) VALUES ("Gary Time", "SpongeSQL, NodeSense, Squidpress, Moustache", "https://Not-a-real-link-haha.com/", "https://i.imgur.com/dOLwZdT.jpg");
+
+INSERT INTO User (User) VALUES ("zuk881");
+
+INSERT INTO User (User) VALUES ("SpongeBob");
