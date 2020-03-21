164abaea259d61896a0a651269fd9e2212a920a1
Rachel Sipes <rasipes@gmail.com>
2020-03-21 14:33:08 -0400

    added config to gitignore again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,20 +2,20 @@ var db = require("../models");
 var path = require("path");
 module.exports = function (app) {
 
-app.get("/", function(req, res) {
-  res.render(path.join(__dirname, "../views/projects.handlebars"));
-});
+  app.get("/", function (req, res) {
+    res.render(path.join(__dirname, "../views/projects.handlebars"));
+  });
 
-// app.get("/cms", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../public/cms.html"));
-// });
+  // app.get("/cms", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/cms.html"));
+  // });
 
-app.get("/projects", function(req, res) {
-  res.render(path.join(__dirname, "../views/projects.handlebars"));
-});
+  app.get("/projects", function (req, res) {
+    res.render(path.join(__dirname, "../views/projects.handlebars"));
+  });
 
-app.get("/user", function(req, res) {
-  res.render(path.join(__dirname, "../public/users.handlebars"));
-});
+  app.get("/user", function (req, res) {
+    res.render(path.join(__dirname, "../public/users.handlebars"));
+  });
 
 };
