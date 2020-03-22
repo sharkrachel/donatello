04b404eeaf665b770560ad004344ce9dd9df0eb3
zuk881 <zuk881@cox.net>
2020-03-22 10:25:49 -0700

    added css file and fixed js errors

--- models/projects.js
+++ models/projects.js
@@ -1,45 +1,53 @@
-module.exports = function (sequelize, DataTypes) {
-  var Projects = sequelize.define("Projects", {
-    projectName: {
-      type: DataTypes.STRING,
-      allowNull: false
+module.exports = function(sequelize, DataTypes) {
+  var Projects = sequelize.define(
+    "Projects",
+    {
+      projectName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      projectTech: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+      projectLink: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+      projectImage: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+      createdAt: {
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        type: DataTypes.DATE
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 22
+      }
     },
-    projectTech: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    projectLink: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    projectImage: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    createdAt: {
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      type: DataTypes.DATE
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 22
-    },
+    {
+      freezeTableName: true, // Model tableName will be the same as the model name
+      underscored: true
+    }
+  );
 
-  }, {
-    freezeTableName: true, // Model tableName will be the same as the model name
-    underscored: true
-  });
-
-
-  Projects.associate = function (models) {
+  Projects.associate = function(models) {
     Projects.belongsTo(models.User, {
       foreignKey: {
         allowNull: false
       }
     });
-  }
+  };
   return Projects;
 };
