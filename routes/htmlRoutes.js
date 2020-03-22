0fbd54bdaa0f281eac9dacc701c045f6d9886fd4
Rachel Sipes <rasipes@gmail.com>
2020-03-22 13:31:16 -0400

    added html pages and updated routes to incorprate html instead of handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,22 +4,22 @@ module.exports = function (app) {
 
   // this is the main page that displays all projects
   app.get("/", function (req, res) {
-    res.render(path.join(__dirname, "../views/projects.handlebars"));
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
   });
 
-  // app.get("/cms", function (req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/cms.html"));
-  // });
+  app.get("/cms", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  });
 
-  // i don't think we need this because our main page displays all the projects, but I commented it out in case we do need it.
 
-  // app.get("/projects", function (req, res) {
-  //   res.render(path.join(__dirname, "../views/projects.handlebars"));
-  // });
+
+  app.get("/projects", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/projects.html"));
+  });
 
   // this page will display all projects for a specific user
   app.get("/user", function (req, res) {
-    res.render(path.join(__dirname, "../public/users.handlebars"));
+    res.sendFile(path.join(__dirname, "../public/users.html"));
   });
 
 };
