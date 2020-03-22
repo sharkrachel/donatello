a8490520ee858653b549ed166cd85d238f939d8e
zuk881 <zuk881@cox.net>
2020-03-22 09:02:08 -0700

    stashing

--- models/projects.js
+++ models/projects.js
@@ -9,7 +9,6 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-
     projectLink: {
       type: DataTypes.STRING,
       allowNull: false
