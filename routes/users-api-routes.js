924b1b67b0e4095b244099c0a9325b51d8f2c54f
zuk881 <zuk881@cox.net>
2020-03-23 16:53:07 -0700

    added code to loop through and display projects/HTML/css for cards/changed title

--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -1,53 +1,48 @@
-var db = require("../models")
-module.exports = function (app) {
-   // GET route to display all projects by a specific user
-    app.get("/api/user", function (req, res) {
-        db.User.findAll({
-            include: [db.Projects]
-        })
-            .then(function (dbUser) {
-                res.json(dbUser);
-            });
+var db = require("../models");
+module.exports = function(app) {
+  // GET route to display all projects by a specific user
+  app.get("/api/user", function(req, res) {
+    db.User.findAll({
+      include: [db.Projects]
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
 
-
-    app.get("/api/user/:username", function (req, res) {
-        db.User.findOne({
-            where: {
-                name: req.params.username
-            }
-
-        }).then(function(dbUser) {
-            res.json(dbUser.id);
-        })
-    })
-
-    app.get("/user/:id", function (req, res) {
-        db.User.findOne({
-            where: {
-                id: req.params.id
-            },
-            include: [dbProjects]
-        }).then(function (dbUser) {
-            res.json(dbUser);
-        });
+  app.get("/api/user/:username", function(req, res) {
+    db.User.findOne({
+      where: {
+        name: req.params.username
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser.id);
     });
+  });
 
-    app.post("/api/user", function (req, res) {
-        db.User.create(req.body)
-            .then(function (dbUser) {
-                res.json(dbUser);
-            });
+  app.get("/user/:id", function(req, res) {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [dbProjects]
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
 
-    app.delete("/api/user/:id", function (req, res) {
-        db.User.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-            .then(function (dbUser) {
-                res.json(dbUser);
-            });
+  app.post("/api/user", function(req, res) {
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
     });
+  });
+
+  app.delete("/api/user/:id", function(req, res) {
+    db.User.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
 };
